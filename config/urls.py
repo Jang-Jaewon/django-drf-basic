2491eb9e1a263dad7bcbcd146628db84794281f1
Jang-Jaewon <jewon119@gmail.com>
2022-02-12 15:48:20 +0900

    Add: Implement CRUD as FBV for Student model using ModelSerializer.

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('firstApp/', include('firstApp.urls')),
+    path('fbvApp/', include('fbvApp.urls')),
 ]
