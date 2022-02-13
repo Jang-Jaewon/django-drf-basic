88ab8cf11b781a096263182039271a36534fb873
Jang-Jaewon <jewon119@gmail.com>
2022-02-13 12:03:18 +0900

    Modify: Implement Student API CRUD using DRF's General APIView

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -7,26 +7,11 @@ from fbvApp.models import Student
 from fbvApp.serializers import StudentSerializer
 
 
-class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class StudentList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request):
-        return self.list(request)
 
-    def post(self, request):
-        return self.create(request)
-
-
-class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
-    def get(self, request, pk):
-        return self.retrieve(request, pk)
-    
-    def put(self, request, pk):
-        return self.update(request, pk)
-    
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
