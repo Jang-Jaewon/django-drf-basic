94b3a4e118310798e2e5cd292fa9bcb91996af34
Jang-Jaewon <jewon119@gmail.com>
2022-02-13 12:21:03 +0900

    Modify: Implement student API CRUD using DRF's ViewSet and DefaultRouter

--- cbvApp/urls.py
+++ cbvApp/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('students', views.StudentViewSet)
 
 urlpatterns = [
-    path('students/', views.StudentList.as_view()),
-    path('students/<int:pk>/', views.StudentDetail.as_view()),
+    path('', include(router.urls))
+    # path('students/', views.StudentList.as_view()),
+    # path('students/<int:pk>/', views.StudentDetail.as_view()),
 ]
--- cbvApp/views.py
+++ cbvApp/views.py
@@ -2,11 +2,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics, mixins
+from rest_framework import viewsets
 
 from fbvApp.models import Student
 from fbvApp.serializers import StudentSerializer
 
 
+class StudentViewSet(viewsets.ModelViewSet):
+    
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+"""
 class StudentList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
@@ -15,3 +22,4 @@ class StudentList(generics.ListCreateAPIView):
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+"""
