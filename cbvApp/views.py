4c210cb5e92b84f1d19061618b2a2f9708a2f26e
Jang-Jaewon <jewon119@gmail.com>
2022-02-15 15:07:34 +0900

    Modify : Create a SearchFilter using the filter provided by rest_framework

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics, mixins
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from fbvApp.models import Student
 from fbvApp.serializers import StudentSerializer
@@ -18,8 +19,8 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     pagination_class = StudentPagination
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'score']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^name', '^score']
     
 
 
